e0e843f462ffec8a049e684fdf1d78b40526312a
PatriziaLandokama9 <patrizia.lando.kama9@gmail.com>
2025-06-23 21:58:03 +0200

    R_cena_candidato_ideale

---
+++ R/R_cena_candidato ideale.R
@@ -0,0 +1,142 @@
+# progetto_ mio festa internazionale 
+# La prima cosa con chi possiamo cenare 
+# Lista di 50 invitati internazionali
+
+Alice_1 <- list("sesso" = "M", "anni" = 31, "nazionalità" = "tedesca", "vacanze" = "città", "vuole_viaggiare" = TRUE)
+James_2 = list("sesso" = "F", "anni" = 48, "nazionalità" = "norvegese", "vacanze" = "mare", "vuole_viaggiare" = TRUE)
+Emily_3 = list("sesso" = "F", "anni" = 25, "nazionalità" = "indonesiana", "vacanze" = "montagna", "vuole_viaggiare" = TRUE)
+Liam_4 = list("sesso" = "M", "anni" = 51, "nazionalità" = "sudafricana", "vacanze" = "montagna", "vuole_viaggiare" = FALSE)
+Olivia_5 = list("sesso" = "F", "anni" = 30, "nazionalità" = "portoghese", "vacanze" = "mare", "vuole_viaggiare" = FALSE)
+Noah_6 = list("sesso" = "F", "anni" = 39, "nazionalità" = "indonesiana", "vacanze" = "mare", "vuole_viaggiare" = TRUE)
+Emma_7 = list("sesso" = "M", "anni" = 34, "nazionalità" = "australiana", "vacanze" = "lago", "vuole_viaggiare" = FALSE)
+William_8 = list("sesso" = "F", "anni" = 57, "nazionalità" = "indonesiana", "vacanze" = "deserto", "vuole_viaggiare" = TRUE)
+Ava_9 = list("sesso" = "F", "anni" = 60, "nazionalità" = "australiana", "vacanze" = "città", "vuole_viaggiare" = FALSE)
+Benjamin_10 = list("sesso" = "M", "anni" = 59, "nazionalità" = "indonesiana", "vacanze" = "città", "vuole_viaggiare" = TRUE)
+Sophia_11 = list("sesso" = "M", "anni" = 68, "nazionalità" = "sudafricana", "vacanze" = "città", "vuole_viaggiare" = TRUE)
+Lucas_12 = list("sesso" = "F", "anni" = 33, "nazionalità" = "americana", "vacanze" = "mare", "vuole_viaggiare" = TRUE)
+Mia_13 = list("sesso" = "M", "anni" = 24, "nazionalità" = "francese", "vacanze" = "deserto", "vuole_viaggiare" = TRUE)
+Henry_14 = list("sesso" = "F", "anni" = 47, "nazionalità" = "canadese", "vacanze" = "città", "vuole_viaggiare" = FALSE)
+Isabella_15 = list("sesso" = "M", "anni" = 31, "nazionalità" = "egiziana", "vacanze" = "mare", "vuole_viaggiare" = TRUE)
+Alexander_16 = list("sesso" = "M", "anni" = 39, "nazionalità" = "giapponese", "vacanze" = "città", "vuole_viaggiare" = TRUE)
+Charlotte_17 = list("sesso" = "F", "anni" = 26, "nazionalità" = "messicana", "vacanze" = "foresta", "vuole_viaggiare" = FALSE)
+Daniel_18 = list("sesso" = "F", "anni" = 49, "nazionalità" = "italiana", "vacanze" = "città", "vuole_viaggiare" = FALSE)
+Amelia_19 = list("sesso" = "F", "anni" = 35, "nazionalità" = "spagnola", "vacanze" = "città", "vuole_viaggiare" = TRUE)
+Michael_20 = list("sesso" = "M", "anni" = 58, "nazionalità" = "iraniana", "vacanze" = "deserto", "vuole_viaggiare" = TRUE)
+Samuel_21 = list("sesso" = "M", "anni" = 41, "nazionalità" = "giapponese", "vacanze" = "mare", "vuole_viaggiare" = TRUE)
+Chloe_22 = list("sesso" = "F", "anni" = 53, "nazionalità" = "keniana", "vacanze" = "lago", "vuole_viaggiare" = FALSE)
+Matthew_23 = list("sesso" = "M", "anni" = 28, "nazionalità" = "cinese", "vacanze" = "foresta", "vuole_viaggiare" = TRUE)
+Victoria_24 = list("sesso" = "F", "anni" = 38, "nazionalità" = "brasiliana", "vacanze" = "montagna", "vuole_viaggiare" = TRUE)
+Joseph_25 = list("sesso" = "M", "anni" = 62, "nazionalità" = "canadese", "vacanze" = "città", "vuole_viaggiare" = FALSE)
+Riley_26 = list("sesso" = "F", "anni" = 44, "nazionalità" = "svedese", "vacanze" = "lago", "vuole_viaggiare" = TRUE)
+Levi_27 = list("sesso" = "M", "anni" = 55, "nazionalità" = "turca", "vacanze" = "mare", "vuole_viaggiare" = TRUE)
+Aria_28 = list("sesso" = "F", "anni" = 31, "nazionalità" = "iraniana", "vacanze" = "foresta", "vuole_viaggiare" = FALSE)
+David_29 = list("sesso" = "M", "anni" = 27, "nazionalità" = "coreana", "vacanze" = "deserto", "vuole_viaggiare" = TRUE)
+Lily_30 = list("sesso" = "F", "anni" = 63, "nazionalità" = "italiana", "vacanze" = "città", "vuole_viaggiare" = TRUE)
+John_31 = list("sesso" = "M", "anni" = 45, "nazionalità" = "spagnola", "vacanze" = "mare", "vuole_viaggiare" = FALSE)
+Aubrey_32 = list("sesso" = "F", "anni" = 50, "nazionalità" = "argentina", "vacanze" = "lago", "vuole_viaggiare" = TRUE)
+Wyatt_33 = list("sesso" = "M", "anni" = 23, "nazionalità" = "nigeriana", "vacanze" = "montagna", "vuole_viaggiare" = TRUE)
+Zoey_34 = list("sesso" = "F", "anni" = 60, "nazionalità" = "americana", "vacanze" = "mare", "vuole_viaggiare" = FALSE)
+Carter_35 = list("sesso" = "M", "anni" = 36, "nazionalità" = "giapponese", "vacanze" = "foresta", "vuole_viaggiare" = TRUE)
+Hannah_36 = list("sesso" = "F", "anni" = 28, "nazionalità" = "francese", "vacanze" = "deserto", "vuole_viaggiare" = TRUE)
+Julian_37 = list("sesso" = "M", "anni" = 47, "nazionalità" = "portoghese", "vacanze" = "città", "vuole_viaggiare" = FALSE)
+Nora_38 = list("sesso" = "F", "anni" = 29, "nazionalità" = "russa", "vacanze" = "montagna", "vuole_viaggiare" = TRUE)
+Grace_39 = list("sesso" = "F", "anni" = 26, "nazionalità" = "turca", "vacanze" = "mare", "vuole_viaggiare" = TRUE)
+Owen_40 = list("sesso" = "M", "anni" = 42, "nazionalità" = "svedese", "vacanze" = "lago", "vuole_viaggiare" = FALSE)
+Scarlett_41 = list("sesso" = "F", "anni" = 33, "nazionalità" = "keniana", "vacanze" = "mare", "vuole_viaggiare" = TRUE)
+Jack_42 = list("sesso" = "M", "anni" = 37, "nazionalità" = "messicana", "vacanze" = "foresta", "vuole_viaggiare" = TRUE)
+Ella_43 = list("sesso" = "F", "anni" = 39, "nazionalità" = "russa", "vacanze" = "montagna", "vuole_viaggiare" = FALSE)
+Sebastian_44 = list("sesso" = "M", "anni" = 61, "nazionalità" = "brasiliana", "vacanze" = "città", "vuole_viaggiare" = TRUE)
+Abigail_45 = list("sesso" = "F", "anni" = 48, "nazionalità" = "nigeriana", "vacanze" = "mare", "vuole_viaggiare" = FALSE)
+Jacob_46 = list("sesso" = "M", "anni" = 52, "nazionalità" = "norvegese", "vacanze" = "deserto", "vuole_viaggiare" = TRUE)
+Evelyn_47 = list("sesso" = "F", "anni" = 34, "nazionalità" = "cinese", "vacanze" = "mare", "vuole_viaggiare" = TRUE)
+Ethan_48 = list("sesso" = "M", "anni" = 29, "nazionalità" = "australiana", "vacanze" = "lago", "vuole_viaggiare" = TRUE)
+Harper_49 = list("sesso" = "F", "anni" = 58, "nazionalità" = "argentina", "vacanze" = "foresta", "vuole_viaggiare" = FALSE)
+Alexander_50 = list("sesso" = "M", "anni" = 27, "nazionalità" = "egiziana", "vacanze" = "deserto", "vuole_viaggiare" = TRUE)
+invitati = list(Alice_1, James_2, Emily_3, Liam_4, Olivia_5, Noah_6, Emma_7, William_8, Ava_9, Benjamin_10, Sophia_11, Lucas_12, Mia_13, Henry_14, Isabella_15, Alexander_16, Charlotte_17, Daniel_18,
+                Amelia_19, Michael_20,Samuel_21, Chloe_22, Matthew_23, Victoria_24, Joseph_25, Riley_26, Levi_27, Aria_28, David_29, Lily_30, John_31, Aubrey_32, Wyatt_33, Zoey_34, Carter_35, Hannah_36, Julian_37, Nora_38, Grace_39, Owen_40,
+                Scarlett_41, Jack_42, Ella_43, Sebastian_44, Abigail_45,Jacob_46, Evelyn_47, Ethan_48, Harper_49, Alexander_50)
+print(invitati)
+##creiamo una lista dei candidati
+candidati = list(
+  Alice_1, James_2, Emily_3, Liam_4, Olivia_5, Noah_6, Emma_7, William_8, Ava_9, Benjamin_10, 
+  Sophia_11, Lucas_12, Mia_13, Henry_14, Isabella_15, Alexander_16, Charlotte_17, Daniel_18,
+  Amelia_19, Michael_20, Samuel_21, Chloe_22, Matthew_23, Victoria_24, Joseph_25, Riley_26,
+  Levi_27, Aria_28, David_29, Lily_30, John_31, Aubrey_32, Wyatt_33, Zoey_34, Carter_35,
+  Hannah_36, Julian_37, Nora_38, Grace_39, Owen_40, Scarlett_41, Jack_42, Ella_43, Sebastian_44,
+  Abigail_45, Jacob_46, Evelyn_47, Ethan_48, Harper_49, Alexander_50
+)
+
+##creiamo una lista dei candidati
+# IL candi dato perfetto  in questo momento sarebbe vedere se troviamo un candindato ideale per proseguire il viaggio 
+candidato_ideale <-list(
+  "sesso"= c('M','F'),
+  "anni"= c(29,30,35,40, 24,20),
+  "nazionalità" = c("canadese", "tedesco", "norvegese", "francese", "italiana" ),
+  "vacanze"=c('città','mare', 'deserto','montagna', 'deserto'),
+  "vuole_viaggiare"=TRUE 
+)
+print(candidato_ideale)
+# Lista vuota per salvare i candidati ideali trovati
+candidati_ideali_trovati <- list()
+
+for (i in 1:length(candidati)) {
+  candidato <- candidati[[i]]
+  
+  if (
+    candidato[["sesso"]] %in% candidato_ideale[["sesso"]] &&
+    candidato[["anni"]] %in% candidato_ideale[["anni"]] &&
+    candidato[["nazionalità"]] %in% candidato_ideale[["nazionalità"]] &&
+    candidato[["vacanze"]] %in% candidato_ideale[["vacanze"]] &&
+    candidato[["vuole_viaggiare"]] == candidato_ideale[["vuole_viaggiare"]]
+  ) {
+    print(paste("Candidato", i, "è ideale!"))
+    print(candidato)
+    
+    # Aggiungi il candidato alla lista
+    candidati_ideali_trovati[[length(candidati_ideali_trovati) + 1]] <- candidato
+  }
+}
+
+# Se vuoi vedere quanti candidati ideali hai trovato
+print(paste("Numero di candidati ideali trovati:", length(candidati_ideali_trovati)))
+
+# GRAFICO 
+# Estrai i sessi dai candidati ideali trovati
+sessi <- sapply(candidati_ideali_trovati, function(x) x[["sesso"]])
+
+# Conta quanti maschi e quante femmine
+table_sessi <- table(sessi)
+
+# Stampa a video il grafico a torta
+pie(table_sessi, main = "Distribuzione sesso candidati ideali", col = c("lightblue", "pink"))
+print(table_nazionalita)
+print(table_sessi)
+    
+sessi_ideali <- sapply(candidati_ideali_trovati, function(x) x[["sesso"]])
+table_sessi <- table(sessi_ideali)
+print(table_sessi)
+# Stampa la lista completa dei candidati ideali
+print(candidati_ideali_trovati)
+
+# Stampa sesso e nazionalità di ogni candidato ideale
+for (i in 1:length(candidati_ideali_trovati)) {
+  candidato <- candidati_ideali_trovati[[i]]
+  print(paste("Candidato", i, "sesso:", candidato[["sesso"]], ", nazionalità:", candidato[["nazionalità"]]))
+}
+
+# Conta quanti candidati ideali per sesso
+sessi_ideali <- sapply(candidati_ideali_trovati, function(x) x[["sesso"]])
+print(table(sessi_ideali))
+
+# Conta quanti candidati ideali per nazionalità
+nazionalita_ideali <- sapply(candidati_ideali_trovati, function(x) x[["nazionalità"]])
+print(table(nazionalita_ideali))
+
+# Estrai i sessi dei candidati ideali
+sessi_ideali <- sapply(candidati_ideali_trovati, function(x) x[["sesso"]])
+
+# Stampa la tabella di frequenza per sesso
+print(table(sessi_ideali))
+sessi_tutti <- sapply(candidati, function(x) x[["sesso"]])
+pie(table(sessi_tutti), main = "Distribuzione sesso candidati", col = c("lightblue", "pink"))
+
